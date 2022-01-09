6711c8c6f14c9ac6503ba06cb7cfedce82346758
Kaolhou <xdroid42@gmail.com>
2022-01-09 18:03:54 -0300

    protótipo quase pronto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import './App.css'
-//import {useEffect, useState} from 'react';
+import NoteContainer from './components/NoteContainer/index'
+import {/*useEffect, */useState} from 'react';
 
 function App() {
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('todo')) || [
+    {
+      createdOn: new Date(),
+      done: false,
+      value: "hello hello"
+    }
+  ])
+
+  const addNote = function(){
+    const inpt = document.getElementById('inpt')
+    if(inpt.value === '') return alert('você deve inserir valores dentro da caixa de texto')
+    const thisNote = {createdOn: new Date(), done: false, value: inpt.value}
+    const allNotes = notes
+    allNotes.push(thisNote)
+    setNotes(allNotes)
+    localStorage.setItem('todo', JSON.stringify(allNotes))
+  }
+  
+  const onDelete = function(key){
+    const allNotes = notes
+    allNotes.splice(key,1)
+    setNotes(allNotes)
+    window.refresh()
+  }
+
+  const notesConteiner = function() {
+    return notes.map((item, key)=>(
+      <NoteContainer
+        key={key}
+        note={item}
+        id={key}
+        onDelete={()=>{onDelete(key)}}
+      />
+    ))
+  }
+
+  console.log(notes)
   return (
-    <div>
-      noa boite
+    <div id="app">
+      {/*input area*/}
+      <form>
+        <input type="text" id="inpt" />
+        <span id='sub' onClick={addNote}>Adicionar</span>
+      </form>
+
+      {/*output area*/}
+      <ul id="out">
+        {notesConteiner()}
+      </ul>
     </div>
   );
 }
---
+++ src/components/NoteContainer/index.js
@@ -0,0 +1,28 @@
+import './index.css'
+
+function NoteContainer({note, id, onDelete}){
+    const createdOn = new Date(note.createdOn)
+    let date = createdOn.getDate()
+    let month = createdOn.getMonth() + 1
+    const year = createdOn.getFullYear()
+
+    if (date < 10) {
+        date = `0${date}`
+      }
+      if (month < 10) {
+        month = `0${month}`
+      }
+
+    return(
+        <li>
+            <div id="header">
+                {`${date}/${month}/${year}`}
+            </div>
+            <div id="content">
+                {note.value}
+            </div>
+        </li>
+    )
+}
+
+export default NoteContainer
